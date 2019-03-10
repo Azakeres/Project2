8944c21f683dd47c42206d08f327569ddc9a8d17
Arash Zakeresfahani <azakeres@iu.edu>
2019-03-10 13:05:22 -0400

    100DaysOfSwift Project 2

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -9,12 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var Button1: UIButton!
+    @IBOutlet var Button2: UIButton!
+    @IBOutlet var Button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        Button1.layer.borderWidth = 1
+        Button2.layer.borderWidth = 1
+        Button3.layer.borderWidth = 1
+        
+        Button1.layer.borderColor = UIColor.lightGray.cgColor
+        Button2.layer.borderColor = UIColor.lightGray.cgColor
+        Button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
+
+        
+
     }
+    func askQuestion(action: UIAlertAction! = nil){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
+        Button1.setImage(UIImage(named: countries[0]) , for: .normal)
+        Button2.setImage(UIImage(named: countries[1]) , for: .normal)
+        Button3.setImage(UIImage(named: countries[2]) , for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
 
-
+    }
+    
+    @IBAction func ButtonTapped(_ sender: UIButton) {
+        
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        }else{
+            title = "WRONG"
+            score -= 1
+        }
+        let ac = UIAlertController(title: title, message: "Your Score is \(score)", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
+    }
+    
 }
 
